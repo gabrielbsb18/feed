8838a394ea7327f1758b8b944eedf38ccbef7bb6
gabrielbsb18 <gabrielrd.corp@gmail.com>
2023-09-24 18:32:56 -0300

    Primeiro commit - Adicionando meu aplicativo React Native

---
+++ script.js
@@ -0,0 +1,13 @@
+function openCardDetail() {
+    // Lógica para redirecionar para a tela de detalhes do card
+    // Você pode usar window.location.href para redirecionar para outra página
+    console.log('Card clicado, redirecionando para a tela de detalhes...');
+  }
+  
+  function openPage(page) {
+    // Lógica para redirecionar para outras páginas
+    // Você pode usar window.location.href para redirecionar para outra página
+    console.log(`Redirecionando para a página ${page}...`);
+  }
+
+  
